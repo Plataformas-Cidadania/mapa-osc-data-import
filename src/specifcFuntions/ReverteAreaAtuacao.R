3aa664f07777476209dacf07a9d36d3c87390d39
Murilojunqueira <m.junqueira@yahoo.com.br>
2024-07-08 17:38:18 -0300

    Rotina para reverter mudanças de tb_area_atuacao

--- src/specifcFuntions/ReverteAreaAtuacao.R
+++ src/specifcFuntions/ReverteAreaAtuacao.R
@@ -13,50 +13,26 @@ library(data.table)
 library(lubridate)
 library(stringr)
 library(assertthat)
+library(DBI)
+library(RODBC)
+library(RPostgres)
+
+# Variável para indicar que a atualização é teste
+
+isTest <- TRUE
 
 # Conexão ####
 
-# Baixa a chave secreta do código
-KeyFile <- "keys/psql12-homolog_keySUPER.json"
-assert_that(file.exists(KeyFile))
-keys <- jsonlite::read_json(KeyFile)
+source("src/generalFunctions/postCon.R")
 
+connec <- postCon("keys/psql12-homolog_keySUPER.json", 
+                  Con_options = "-c search_path=osc")
 
-# Verifica se pode conectar
-TestConexao <- dbCanConnect(RPostgres::Postgres(), 
-                            dbname = keys$dbname,
-                            host = keys$host,
-                            port = keys$port,
-                            user = keys$username, 
-                            password = keys$password,
-                            options="-c search_path=osc")
-
-assert_that(TestConexao, 
-            msg = paste("O teste de coneção falhou, ", 
-                        "verificar nome da base, host, ",
-                        "porta, usuário e senha."))
-
-# conencta à base
-connec <- dbConnect(RPostgres::Postgres(), 
-                    dbname = keys$dbname,
-                    host = keys$host,
-                    port = keys$port,
-                    user = keys$username, 
-                    password = keys$password,
-                    options="-c search_path=osc")
-
-# Verifica a coneção com a base
-assert_that(dbIsValid(connec))
-
-rm(keys, TestConexao, KeyFile)
-
+rm(postCon)
 
 # Reverte Atualização ####
 
-
-# Limpar os dados colocados na atualização antiga (fazer um script só para isso)
-
-### Evitar limpar dados dos usuários
+# Limpar os dados colocados na atualização antiga
 
 ## Baixar a base atual de tb_areas_atualizacao
 # OldData <- readRDS("D:/Users/Murilo/Dropbox/ActiveProjects/IPEAGeral/data/temp/2024-04-09 Extracao MOSC/tb_area_atuacao.RDS")
@@ -66,89 +42,144 @@ OldData <- try(dbGetQuery(connec,
                         paste0("SELECT * FROM tb_area_atuacao",
                                # " LIMIT 500", 
                                ";")))
+# Checa a base:
+## names(OldData)
+## table(OldData$ft_area_atuacao)
 
-names(OldData)
-
-table(OldData$ft_area_atuacao)
+# Ao invés te atualizar a base mesmo, testar nessa cópia:
+if(isTest) {
+  if(dbExistsTable(connec, "tb_area_atuacao_teste")) dbRemoveTable(connec, "tb_area_atuacao_teste")
+  dbWriteTable(connec, "tb_area_atuacao_teste", OldData)
+  
+  NomeTabelaAtt <- "tb_area_atuacao_teste"
+} else {
+  NomeTabelaAtt <- "tb_area_atuacao"
+}
 
 ## Ver pela fonte de atualização os dados que foram inseridos na última att e limpar.
-## (usar a chave: "id_osc & cd_area_atuacao & cd_subarea_atuacao")
-
-CleanData <- OldData %>% 
-  mutate(IsUser = ifelse(ft_area_atuacao == "Representante de OSC", 1, 0)) %>% 
-  group_by(id_osc, cd_area_atuacao, cd_subarea_atuacao) %>% 
-  arrange(desc(IsUser)) %>% 
-  slice(1)
-
-table(CleanData$ft_area_atuacao)
-
 
 DeleteRow <- OldData %>% 
-  mutate(IsUser = ifelse(ft_area_atuacao == "Representante de OSC", 1, 0)) %>% 
+  # Usar a chave: ("id_osc & cd_area_atuacao & cd_subarea_atuacao")
   group_by(id_osc, cd_area_atuacao, cd_subarea_atuacao) %>% 
+  # Evitar limpar dados dos usuários
+  mutate(IsUser = ft_area_atuacao == "Representante de OSC") %>% 
   arrange(IsUser) %>% 
+  # Determinar que só uma linha da combinação de chaves pode ficar
   mutate(n = row_number(), 
-         deleterow = ifelse(n > 1, 1, 0)) %>% 
+         deleterow = n > 1) %>% 
   ungroup() %>% 
   select(id_area_atuacao, deleterow) %>% 
   select(everything())
 
-table(DeleteRow$n)
-table(DeleteRow$deleterow)
+# Checa os dados:
+## table(DeleteRow$deleterow)
 
-## Faz upload da tabela temporária com as linhas de atualização
-
+# Deleta os dados usando uma entidade de dados temporária
 if(dbExistsTable(connec, "update_temp")) dbRemoveTable(connec, "update_temp")
-
 dbWriteTable(connec, "update_temp", DeleteRow)
+dbExistsTable(connec, "update_temp") # checa criação de tabela
 
-# teste <- dbGetQuery(connec, "SELECT * FROM update_temp")
-# View(teste)
-# table(teste$temp_var, useNA = "always")
+# Testa para ver se a base temporára está correta:
+teste <- dbGetQuery(connec, "SELECT * FROM update_temp")
+table(teste$deleterow, useNA = "always")
+rm(teste)
 
-# Faz umm join desta tabela com os dados antigos
-
-# Deleta a coluna, se ela existir:
-query_DropCol <- paste0("ALTER TABLE tb_area_atuacao",
+# Deleta a coluna que indica os dados que vão ser deletados (se precisar):
+query_DropCol <- paste0("ALTER TABLE ", NomeTabelaAtt,
                         " DROP COLUMN IF EXISTS deleterow;")
-
+# cat(query_DropCol)
 dbExecute(connec, query_DropCol)
 
-# Pega o tipo de variável a ser criado:
-QueryGetInformation <- paste0("SELECT *", "\n",
-                              "FROM information_schema.columns", "\n",
-                              "WHERE table_name = 'tb_area_atuacao';")
-# cat(QueryGetInformation)
-ColTypes <- dbGetQuery(connec, QueryGetInformation)
-VarType <- ColTypes$data_type[ColTypes$column_name == "deleterow"]
-
+# Cria coluna na tabela de dados antigos
+query_AddCol <- paste0("ALTER TABLE ", NomeTabelaAtt,
+                       " ADD COLUMN deleterow boolean",
+                       ";")
+# cat(query_AddCol)
 dbExecute(connec, query_AddCol)
+rm(query_AddCol)
 
-rm(QueryGetInformation, ColTypes, VarType, query_AddCol)
+# Testa para ver se a coluna foi inserida:
+teste2 <- try(dbGetQuery(connec, 
+                          paste0("SELECT * FROM ", NomeTabelaAtt,
+                                 " LIMIT 500", 
+                                 ";")))
+assert_that("deleterow" %in% names(teste2))
+rm(teste2)
 
-# Insere a coluna com as linhas para deletar
-query_JoinUpdate <- paste0("DELETE FROM tb_area_atuacao", "\n",
-                           "  WHERE deleterow = 1",
+# Insere a referência das linhas que vão ser inseridas:
+query_JoinUpdate <- paste0("UPDATE ", NomeTabelaAtt, "\n",
+                           " SET deleterow = update_temp.deleterow", "\n",
+                           " FROM update_temp", "\n",
+                           " WHERE ", NomeTabelaAtt, ".id_area_atuacao",  
+                           " = update_temp.id_area_atuacao", 
                            ";")
-
 # cat(query_JoinUpdate)
-
-# Executa a inserção das colunas
 dbExecute(connec, query_JoinUpdate)
 
-# Determina a atualização da coluna com base no valor de temp_var
-query_UpdateCol <- paste0("UPDATE ", Table_NameAntigo, "\n",
-                          " SET ", col, " = temp_var",
-                          ";")
-# cat(query_UpdateCol)
+teste3 <- try(dbGetQuery(connec, 
+                         paste0("SELECT * FROM ", NomeTabelaAtt,
+                                # " LIMIT 500", 
+                                ";")))
 
-# query_DeleteRows
-LinhasAtualizadas <- dbExecute(connec, query_UpdateCol)
+names(teste3)
+head(teste3)
+table(teste3$deleterow, useNA = "always")
+rm(teste3)
 
-message("Linhas atualizadas na coluna ", col)
+rm(query_JoinUpdate)
 
-# Deleta a coluna criada
+# Insere a coluna com as linhas para deletar
+query_Delete <- paste0("DELETE FROM ", NomeTabelaAtt, "\n",
+                           "  WHERE deleterow",
+                           ";")
+# cat(query_Delete)
+
+# Deleta as linhas
+dbExecute(connec, query_Delete)
+
+# Verifica se deu tudo certo
+teste4 <- try(dbGetQuery(connec, 
+                         paste0("SELECT * FROM ", NomeTabelaAtt,
+                                # " LIMIT 500", 
+                                ";")))
+
+names(teste4)
+nrow(teste4)
+table(teste4$deleterow, useNA = "always")
+rm(teste4)
+
+rm(query_Delete)
+
+# Retira a coluna temporária para indicar as linhas a se deletar
 dbExecute(connec, query_DropCol)
+rm(query_DropCol)
 
+# Vou deixar aqui opção de fazer uma cópia do output do teste
+if(isTest) {
+  # Verifica se deu tudo certo
+  NewTable <- try(dbGetQuery(connec, 
+                             paste0("SELECT * FROM ", NomeTabelaAtt,
+                                    # " LIMIT 500", 
+                                    ";")))
+  
+  DirExtract <- "data/temp/2024-07-08 extract/"
+  if(!dir.exists(DirExtract)) {
+    dir.create(DirExtract)
+  }
+  saveRDS(NewTable, paste0(DirExtract, NomeTabelaAtt, ".RDS"))
+  rm(DirExtract, NewTable)
+}
+
+# Remove tabela temporária de dados
+if(dbExistsTable(connec, "update_temp")) dbRemoveTable(connec, "update_temp")
+dbExistsTable(connec, "update_temp") # checa extinção de tabela
+
+# Desliga conexão
+dbDisconnect(connec)
+
+# Limpa memória
+rm(connec, isTest, NomeTabelaAtt)
+rm(DeleteRow, OldData)
+ls()
 
 # Fim ####
