b2bdd605b61dbae03d9a549f4dcd3595fb86423e
Murilojunqueira <m.junqueira@yahoo.com.br>
2024-11-21 11:37:33 -0300

    Pequenas Atualizações

--- src/testes/TestaNovaClassificacaoAreaAtuacao.R
+++ src/testes/TestaNovaClassificacaoAreaAtuacao.R
@@ -9,22 +9,23 @@
 
 library(readxl)
 library(lubridate)
+library(stringr)
 
 # Função para determinar as áreas de atuação
 source("src/specificFunctions/AreaAtuacaoOSC.R")
 
 
-Tb_OSC_Full <- readRDS("backup_files/2023_01/intermediate_files/Tb_OSC_Full.RDS")
+Tb_OSC_Full <- readRDS("backup_files/2024_01/intermediate_files/Tb_OSC_Full.RDS")
 
 # names(Tb_OSC_Full)
 
 # Transforma Tb_OSC_Full em DB_OSC
 DB_OSC <- Tb_OSC_Full %>%
-  # mutate(cnae = .data[["cnae_fiscal"]], 
-  mutate(cnae = .data[["cnae_fiscal_principal"]], 
+  mutate(cnae = .data[["cnae_fiscal"]], 
+  # mutate(cnae = .data[["cnae_fiscal_principal"]], 
          micro_area_atuacao = NA)
 
-DB_OSC <- slice(DB_OSC, 1:100000)
+# DB_OSC <- slice(DB_OSC, 1:100000)
 
 # names(DB_OSC)
 
@@ -39,10 +40,10 @@ DB_SubAreaRegras <- read_xlsx("tab_auxiliares/IndicadoresAreaAtuacaoOSC.xlsx",
 DB_AreaSubaria <- read_xlsx("tab_auxiliares/IndicadoresAreaAtuacaoOSC.xlsx", 
                             sheet = "Areas&Subareas")
 
-names(DB_SubAreaRegras)
+# names(DB_SubAreaRegras)
 
-DB_SubAreaRegras <- DB_SubAreaRegras %>% 
-  dplyr::filter(AreaAtuacao != "Desenvolvimento rural")
+# DB_SubAreaRegras <- DB_SubAreaRegras %>% 
+#   dplyr::filter(AreaAtuacao != "Desenvolvimento rural")
 
 # rm(Tb_OSC_Full) # não vamos mais utilizar esses dados
 
@@ -66,7 +67,7 @@ DB_OSC <- DB_OSC %>%
   # Insere Macro áreas de atuação
   left_join(DB_AreaSubaria, by = "micro_area_atuacao")
 
-now()
+# now()
 
 DB_OSC %>% 
   group_by(micro_area_atuacao) %>% 
@@ -74,11 +75,111 @@ DB_OSC %>%
   mutate(Per = Freq/sum(Freq)) %>% 
   print(n = 40)
 
-DB_OSC$razao_social[DB_OSC$micro_area_atuacao == "Outras formas de desenvolvimento e defesa de direitos"] %>% 
-  sample(20)
+# DB_OSC$razao_social[DB_OSC$micro_area_atuacao == "Outras formas de desenvolvimento e defesa de direitos"] %>% 
+#   sample(20)
 
-# To DO:
-# Fazer um comparativo entre o critério anterior e esse aqui.
+
+# Comparativo entre o critério anterior e o presente:
+
+# Baixa dados da última atualização:
+
+getwd()
+
+setwd(rstudioapi::getActiveProject())
+
+tb_area_atuacao_2024 <- readRDS("backup_files/2024_01/output_files/tb_area_atuacao.RDS")
+
+## Tabelas auxiliares:
+dc_area_atuacao <- fread("tab_auxiliares/dc_area_atuacao.csv", encoding = "Latin-1")
+dc_subarea_atuacao <- fread("tab_auxiliares/dc_subarea_atuacao.csv", encoding = "Latin-1") %>% 
+  distinct(cd_subarea_atuacao, .keep_all = TRUE)
+# names(dc_subarea_atuacao)
+
+# Compara dados de área de atuação:
+
+JoinData <- tb_area_atuacao_2024 %>% 
+  dplyr::filter(ft_area_atuacao == "AreaAtuacaoOSC.R_2024_01") %>% 
+  left_join(dc_area_atuacao, by = "cd_area_atuacao") %>% 
+  group_by(tx_area_atuacao) %>% 
+  summarise(Freq_OLD = n()) %>% 
+  mutate(Per_OLD = (Freq_OLD/sum(Freq_OLD)) * 100)
+
+# names(DB_OSC)
+
+AreaTable <- DB_OSC %>% 
+  rename(tx_area_atuacao = macro_area_atuacao) %>% 
+  group_by(tx_area_atuacao) %>% 
+  summarise(Freq = n()) %>% 
+  mutate(Per = (Freq/sum(Freq)) * 100) %>% 
+  full_join(JoinData, by = "tx_area_atuacao") %>% 
+  mutate(Diferenca = Freq - Freq_OLD)
+
+AreaTable
+
+
+# Compara dados de área de SubArea de atuação:
+
+JoinData <- tb_area_atuacao_2024 %>% 
+  dplyr::filter(ft_area_atuacao == "AreaAtuacaoOSC.R_2024_01") %>% 
+  dplyr::filter( !is.na(cd_subarea_atuacao) ) %>% 
+  left_join(dc_subarea_atuacao, by = "cd_subarea_atuacao") %>% 
+  group_by(tx_subarea_atuacao) %>% 
+  summarise(Freq_OLD = n()) %>% 
+  mutate(Per_OLD = (Freq_OLD/sum(Freq_OLD)) * 100)
+
+# names(DB_OSC)
+
+SubAreaTable <- DB_OSC %>% 
+  rename(tx_subarea_atuacao = micro_area_atuacao) %>% 
+  group_by(tx_subarea_atuacao) %>% 
+  summarise(Freq = n()) %>% 
+  mutate(Per = (Freq/sum(Freq)) * 100) %>% 
+  full_join(JoinData, by = "tx_subarea_atuacao") %>% 
+  mutate(Diferenca = Freq - Freq_OLD)
+
+SubAreaTable %>% 
+  print(n = 40)
+
+
+# Compara dados de área de Defesa de Direitos:
+
+JoinData <- tb_area_atuacao_2024 %>% 
+  dplyr::filter(ft_area_atuacao == "AreaAtuacaoOSC.R_2024_01") %>% 
+  dplyr::filter( cd_area_atuacao == 9 ) %>% 
+  left_join(dc_subarea_atuacao, by = "cd_subarea_atuacao") %>% 
+  group_by(tx_subarea_atuacao) %>% 
+  summarise(Freq_OLD = n()) %>% 
+  mutate(Per_OLD = (Freq_OLD/sum(Freq_OLD)) * 100)
+
+# names(DB_OSC)
+
+table(DB_OSC$macro_area_atuacao)
+
+DefesaDireitos <- DB_OSC %>% 
+  rename(tx_subarea_atuacao = micro_area_atuacao) %>% 
+  dplyr::filter(macro_area_atuacao == "Desenvolvimento e defesa de direitos e interesses") %>% 
+  group_by(tx_subarea_atuacao) %>% 
+  summarise(Freq = n()) %>% 
+  mutate(Per = (Freq/sum(Freq)) * 100)
+
+DefesaDireitos %>% 
+  print(n = 40)
+
+# Salva dados:
+
+fwrite(AreaTable, 
+       "D:/Users/Murilo/Dropbox/Trabalho/IPEA/SubProjetos/2024-08 Fasfil/AreaTable.csv",
+       sep = ";", dec = ",")
+
+fwrite(SubAreaTable, 
+       "D:/Users/Murilo/Dropbox/Trabalho/IPEA/SubProjetos/2024-08 Fasfil/SubAreaTable.csv",
+       sep = ";", dec = ",")
+
+fwrite(DefesaDireitos, 
+       "D:/Users/Murilo/Dropbox/Trabalho/IPEA/SubProjetos/2024-08 Fasfil/DefesaDireitos.csv",
+       sep = ";", dec = ",")
+
+
 
 
 # Fim ####
